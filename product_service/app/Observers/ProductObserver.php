3f14ff132af9e151f8e4abbcdcadb04a3a984e49
David Petter <cfdavidpetter@gmail.com>
2020-01-10 00:29:17 -0200

    Create product consumer on Kafka

---
+++ product_service/app/Observers/ProductObserver.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Observers;
+
+use App\Product;
+
+class ProductObserver
+{
+    /**
+     * Handle the product "created" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function created(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the product "updated" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function updated(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the product "deleted" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function deleted(Product $product)
+    {
+        //
+    }
+}
