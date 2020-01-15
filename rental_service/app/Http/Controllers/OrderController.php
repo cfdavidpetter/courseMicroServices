290327a1c967327c50f3436f88b6d9ce0f959f1d
David Petter <cfdavidpetter@gmail.com>
2020-01-15 01:26:14 -0200

    Create order controller

---
+++ rental_service/app/Http/Controllers/OrderController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Order;
+use Ramsey\Uuid\Uuid;
+use App\Http\Requests\OrderRequest;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Order::with('items')->paginate(15), 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json(Order::with('items')->findOrFail($id), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(OrderRequest $request)
+    {
+        return response()->json(Order::create(
+            array_merge($request->all(), ['id' => Uuid::uuid4()])
+        ), 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $Order = Order::findOrFail($id);
+        $Order->fill($request->all());
+        $Order->save();
+
+        return response()->json($Order, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return response()->json(Order::destroy($id), 200);
+    }
+}
---
+++ rental_service/app/Http/Requests/OrderRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Custom\CustomFormRequest;
+
+class OrderRequest extends CustomFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'          => 'required|max:255',
+            'description'   => 'required|max:255',
+            'price'         => 'required',
+            'qtd_available' => 'required',
+            'qtd_total'     => 'required',
+        ];
+    }
+}
